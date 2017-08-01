5b50440ddf4eb10dcb140b334d90bd59c23699e4
JamesORourke <geekjamesorourkejr@icloud.com>
2017-07-31 17:58:30 -0700

    Assignment done

---
+++ FindCharacters.py
@@ -0,0 +1,31 @@
+"""
+Assignment: Find Characters
+Write a program that takes a list of strings and a string containing a single
+character, and prints a new list of all the strings containing that character.
+"""
+
+def FindCharacters(aListOfStrings, LookForChar):
+    #we could use Reduce here, except we are not allowed.
+    print "word_list = {0}".format(aListOfStrings)
+    print "char = {0}".format(LookForChar)
+    returnList = []
+    for aWord in aListOfStrings:
+        for ch in aWord:
+            if (str(ch) == LookForChar) :
+                returnList.append(aWord)
+                break;
+    print "new_list = {0}".format(returnList)
+
+
+"""
+Here's an example:
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+"""
+
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+FindCharacters(word_list, char)
